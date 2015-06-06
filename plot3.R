6f2d9a3f25c98cfb0a9f816db57d5c0741fe3b85
seaem <seaem@users.noreply.github.com>
2015-06-06 23:30:43 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -18,7 +18,7 @@ lines(datetime,submetering1)
 lines(datetime,submetering2,col="red")
 lines(datetime,submetering3,col="blue")
 
-legend("topright",lty=1,col=c("black","red","blue"),legend=c("sub_metering_1","sub_metering_2","sub_metering_3"),cex=0.5)
+legend("topright",lty=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),cex=0.75)
 
 #png file
 dev.copy(png,file="plot3.png")
